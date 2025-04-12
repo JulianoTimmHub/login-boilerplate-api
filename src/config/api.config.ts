cb06e382d28a0b094d3f56f39049acc6bf758082
JulianoTimmHub <julianohenriquetimm@gmail.com>
2025-04-12 02:37:15 -0300

    fix: changing cors configuration and api config

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,16 +1,25 @@
 export type ApiConfig = {
   port: number;
   basePath: string;
+  externalUrl: string;
+  methods: string | string[];
+  allowedHeaders: string | string[];
 }
 
 const {
   API_PORT: port,
-  API_BASE_PATH: basePath
+  API_BASE_PATH: basePath,
+  API_EXTERNAL_URL: externalUrl,
+  API_METHODS: methods,
+  API_ALLOWED_HEADERS: allowedHeaders
 } = process.env;
 
 export const apiConfig = (): { api: ApiConfig } => ({
   api: {
     port: parseInt(port, 10),
     basePath: basePath,
+    externalUrl: externalUrl,
+    methods: methods.split(','),
+    allowedHeaders: allowedHeaders.split(',')
   }
 });
